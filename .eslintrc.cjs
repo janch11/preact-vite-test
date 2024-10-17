5af66ddbad33f44c879f058267bc62051e594f4c
jchoma <jandominikchoma@gmail.com>
2024-10-17 13:49:28 +0200

    add eslint config

---
+++ .eslintrc.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    extends: ['preact'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    rules: {
+      // Add any custom rules here
+    },
+  };
